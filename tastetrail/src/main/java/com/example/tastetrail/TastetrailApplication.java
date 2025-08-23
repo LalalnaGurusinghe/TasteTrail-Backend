d1b91c57ef17fa80ef8f03c9154cbc872a207e66
Lalana Malintha Gurusinghe <153848846+LalalnaGurusinghe@users.noreply.github.com>
2025-08-23 17:07:21 +0530

    Initialize Spring Boot project for TasteTrail
    Add initial Spring Boot project structure for the TasteTrail application, including Maven build files, application entry point, basic configuration, and a context load test. Includes support for web, security, OAuth2 resource server, validation, and Elasticsearch, with Lombok and test dependencies.

---
+++ tastetrail/src/main/java/com/example/tastetrail/TastetrailApplication.java
@@ -0,0 +1,14 @@
+package com.example.tastetrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastetrailApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(TastetrailApplication.class, args);
+	}
+
+}
---
+++ tastetrail/src/test/java/com/example/tastetrail/TastetrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tastetrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastetrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
